9d962de9f469becc3104945bdfb17e565393aa47
finaritrandrianiaina@gmail.com <andrianiaina@localhost>
2021-02-26 10:17:00 +0300

    create who amo

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Param, ParseUUIDPipe, Post, Put, Request, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseUUIDPipe, Post, Put, Req, Request, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -23,25 +23,31 @@ export class UsersController {
 	create(@Body() createUserDto: CreateUserDto): Promise<User> {
 		return this.usersService.create(createUserDto);
 	}
-	
+
 	@Get()
 	@ApiBearerAuth()
 	@Roles(ADMIN)
 	async findAll(@Request() req) {
 		return await this.usersService.findAll();
 	}
-	
+
 	@Get(':id')
 	@AllowAnonymous()
 	async findOne(@Param('id', ParseUUIDPipe) id: string) {
 		return await this.usersService.findOne(id);
 	}
-	
+
 	@Put(':id')
 	@ApiBearerAuth()
 	@OwnerOrAdmin()
 	update(@Param('id', ParseUUIDPipe) id: string, @Body() updateUserDto: UpdateUserDto) {
 		return this.usersService.update(id, updateUserDto);
+	}
+
+	@Get('whoami')
+	@ApiBearerAuth()
+	whoami(@Req() req) {
+		return req.user;
 	}
 	
 	@Delete(':id')
